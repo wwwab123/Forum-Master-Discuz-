80e21b93b99c6a9da6a4dcc8162d1bfd67ab2bc0
wwwab <94147274+wwwab123@users.noreply.github.com>
2024-10-14 20:20:18 +0800

    1.1.4

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // @name:zh-CN   论坛大师・Discuz！修改版
 // @name:zh-TW   論壇大師・Discuz！修改版
 // @namespace    Forum Master・Discuz!-mxdh (Update by wwwab)
-// @version      1.1.3
+// @version      1.1.4
 // @icon         https://discuz.dismall.com/favicon.ico
 // @description  Forum Master - Discuz!　Beautify the interface, Remove ads, Enhance functions.
 // @description:en    Forum Master - Discuz!　Beautify the interface, Remove ads, Enhance functions.
@@ -761,7 +761,7 @@
         display_lock_skin_button = true;
     }
 
-    if (lock_skin === true) {
+    if (lock_skin) {
         if (site === 'KAFAN') {
             let cssLink1 = document.createElement('link');
             cssLink1.rel = 'stylesheet';
@@ -1220,5 +1220,5 @@
                 -webkit-user-select: none;
                 user-select: none;
             }
-        `);
+    `);
 })();
